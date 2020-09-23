4f87aff8e3d1fc88dbf4036382d21f5f37ec2257
WN-Bronx <dj.pretto@hotmail.com>
2020-09-23 12:00:52 -0300

    Commit

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,48 @@
 <template>
-  <div id="app">
-  </div>
+  <div class="principal">
+    <div id="app">
+      <img class="img-responsive img-logo" src="@/assets/photo.png" alt="Logo">
+      <h1>SORTEIO do BRONX</h1>
+      <div v-for="(AddPessoa,index) in add" :key="add.id">
+        <h6>{{index + 1 }}
+        <AddPessoa :add="add"/>
+      </div>
+    </div>
+   <div>
+   </div>
+  </div>  
 </template>
 
 <script>
 
+import AddPessoa from './components/AddPessoa'
 
 export default {
   name: 'App',
+  data(){
+    return {
+
+    }
+  },
   components: {
-  }
+    AddPessoa,
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .img-logo{
+    max-width: 120px;
+    margin: 0 auto;
+  }
 </style>
---
+++ src/components/AddPessoa.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="div-principal">
+        <div>
+            <h2 class="h2">- Start the GAME! -</h2>
+        </div>
+        <input class="input-form" type="text" v-model="nomeField" id="name" placeholder="Informe seu Name">
+        <input class="input-form" type="email" v-model="emailField" id="name" placeholder="Informe seu E-mail">
+        
+        <button @click="adicionarPessoa" class="btn-style">Add Informação</button>
+    </div>
+            
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            nomeField: "",
+            emailField: "",
+            add: [ 
+                {
+                    id: "",
+                    nome: "",
+                    email: ""
+                } 
+            ]
+        }    
+    },
+
+    methods: {
+        adicionarPessoa: function() {
+        this.add.push({nome: this.nomeField, email:this.emailField, id:Date.now()})
+        this.nomeField = "";
+        this.emailField = "";
+    }
+  }
+}
+</script>
+
+<style scoped>
+    .div-principal{
+        text-align: center;
+        background-color: darkcyan;
+        color: white;
+        max-width: 950px;
+        margin: 0 auto;
+        border: 2px solid black ;
+    }
+
+    .h2{
+        color: black;
+        font-family: 'Courier New', Courier, monospace;
+        font-size: 20px;
+        font-style: oblique;
+    }
+
+    .input-form{
+        max-width: 250px;
+        min-width: 50px;
+        text-align: center;
+        margin-left: 15px;
+        margin-bottom: 15px;
+        padding: 5px 5px 5px 5px;
+        border: 1px solid black;
+    }
+
+    .btn-style{
+        text-align: center;
+        max-width: 150px;
+        margin: 5px 5px 5px;
+        padding: 5px 5px 5px 2px;
+    }
+</style>
